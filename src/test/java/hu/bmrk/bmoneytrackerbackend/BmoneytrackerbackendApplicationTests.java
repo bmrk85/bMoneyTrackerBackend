1c435704dd99cd10cc353e4a46b403d1efb718a3
Mark Banu <mark.banu@clarmont.hu>
2020-02-13 09:35:20 +0100

    initial commit

---
+++ src/test/java/hu/bmrk/bmoneytrackerbackend/BmoneytrackerbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.bmrk.bmoneytrackerbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BmoneytrackerbackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
