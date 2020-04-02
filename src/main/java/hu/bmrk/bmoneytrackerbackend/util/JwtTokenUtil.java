b42b955b973ae186294028434e2df7e8b6f56cd7
Mark Banu <mark.banu@clarmont.hu>
2020-04-02 16:47:24 +0200

    test pg added

--- src/main/java/hu/bmrk/bmoneytrackerbackend/util/JwtTokenUtil.java
+++ src/main/java/hu/bmrk/bmoneytrackerbackend/util/JwtTokenUtil.java
@@ -1,10 +1,8 @@
 package hu.bmrk.bmoneytrackerbackend.util;
 
-import hu.bmrk.bmoneytrackerbackend.service.interfaces.UserEntityService;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
@@ -17,9 +15,6 @@ import java.util.function.Function;
 
 @Component
 public class JwtTokenUtil implements Serializable {
-
-    @Autowired
-    UserEntityService userEntityService;
 
     private static final long JWT_TOKEN_VALIDITY = 5 * 60 * 60;
 
