0b8cff3910357b37f34680d29683d35b70a9eabb
Mark Banu <mark.banu@clarmont.hu>
2020-04-02 18:01:54 +0200

    testing automerge

--- src/main/java/hu/bmrk/bmoneytrackerbackend/BmoneytrackerbackendApplication.java
+++ src/main/java/hu/bmrk/bmoneytrackerbackend/BmoneytrackerbackendApplication.java
@@ -1,6 +1,12 @@
 package hu.bmrk.bmoneytrackerbackend;
 
-/*@SpringBootApplication
+import org.modelmapper.ModelMapper;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Scope;
+
+@SpringBootApplication
 public class BmoneytrackerbackendApplication {
 
     @Bean
@@ -13,4 +19,4 @@ public class BmoneytrackerbackendApplication {
         SpringApplication.run(BmoneytrackerbackendApplication.class, args);
     }
 
-}*/
+}
